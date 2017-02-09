21341097becf611725a5abd84942a25d9a8c9a80
yuandongzhong <2409056945@qq.com>
2017-02-09 22:17:12 +0800

    2nd Commit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,21 +3,21 @@ import Vapor
 let drop = Droplet()
 
 
-/*
+
 drop.get("/name",":name") { request in
     if let name = request.parameters["name"]?.string {
         return "Hello \(name)!"
     }
     return "Error retrieving parameters."
 }
- */
 
 
-/*
+
+
 drop.get("/") { request in
     return "Hello World!"
 }
- */
+
 
 
 drop.get("/view") { request in
